a038d134ba3d20b2d98ffffbc541bb79f18daf85
Leonardo <leo.spaula@gmail.com>
2015-07-07 11:14:13 -0300

    Adds validations to store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,15 @@
 class Store < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
   mount_uploader :logo, LogoUploader
+
+  validates :name, :address, :telephone, :cnpj, :business_hours, presence: true
+
+  validates :delivery_hours, presence: true, if: :delivers?
+
+  private
+  def delivers?
+    delivers
+  end
 end
