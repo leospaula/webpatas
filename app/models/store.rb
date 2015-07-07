5e73419ef4ca3df68b5f61f42ce9f9625822f684
Leonardo <leo.spaula@gmail.com>
2015-07-07 11:04:28 -0300

    Creates logo uploader and mounts on store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,5 @@
 class Store < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  mount_uploader :logo, LogoUploader
 end
