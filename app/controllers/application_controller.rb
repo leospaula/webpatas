ebddc5a737c709960bdf36f1c7c4397d9aabaef1
Leonardo <leo.spaula@gmail.com>
2015-07-07 15:26:59 -0300

    Adds extra fields to registration forms also permits it's parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :address, :telephone,
+                                                :website, :description, :logo,
+                                                :accept_credit_card,
+                                                :accept_debit_card, :delivers,
+                                                :delivery_hours, :business_hours,
+                                                :cnpj]
+
+    devise_parameter_sanitizer.for(:account_update) << [:name, :address, :telephone,
+                                                :website, :description, :logo,
+                                                :accept_credit_card,
+                                                :accept_debit_card, :delivers,
+                                                :delivery_hours, :business_hours,
+                                                :cnpj]
+  end
 end
