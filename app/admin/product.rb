cbdff9f4971f6b368f09fb93087215decb5b2ed9
Leonardo <leo.spaula@gmail.com>
2015-07-12 20:26:16 -0300

    Adds product images to admin product show

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -15,6 +15,29 @@ ActiveAdmin.register Product do
     actions
   end
 
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :technical_information
+      row :brand
+      row :reference
+      row :sku
+      row :image_1 do |product|
+        image_tag product.image_1.url, size: '150x150'
+      end
+      row :image_2 do |product|
+        image_tag product.image_1.url, size: '150x150'
+      end
+      row :image_3 do |product|
+        image_tag product.image_1.url, size: '150x150'
+      end
+      row :image_4 do |product|
+        image_tag product.image_1.url, size: '150x150'
+      end
+    end
+  end
+
   form do |f|
     f.semantic_errors
     f.inputs do
