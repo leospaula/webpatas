4712c98525a1872aecf68ee12e8ce1cfb09d2370
Leonardo <leo.spaula@gmail.com>
2015-07-13 16:33:22 -0300

    Validates category and name on sub_category model

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -2,6 +2,8 @@ class SubCategory < ActiveRecord::Base
   belongs_to :category
   has_and_belongs_to_many :products
 
+  validates :name, :category, presence: true
+
   def display_name
     "#{category.name} - #{name}"
   end
